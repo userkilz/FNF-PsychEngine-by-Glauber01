7b1f2f2eb8e0c53c1be0f282445e75aa98d4cd3c
userkilz <141530435+userkilz@users.noreply.github.com>
2023-08-17 13:35:14 -0300

    Add files via upload

--- source/Controls.hx
+++ source/Controls.hx
@@ -13,7 +13,6 @@ import flixel.input.keyboard.FlxKey;
 #if mobile
 import mobile.flixel.FlxButton;
 import mobile.flixel.FlxHitbox;
-import mobile.flixel.FlxNewHitbox;
 import mobile.flixel.FlxVirtualPad;
 #end
 
@@ -48,7 +47,6 @@ enum abstract Action(String) to String from String
 	var BACK = "back";
 	var PAUSE = "pause";
 	var RESET = "reset";
-	var SPACE = "space";
 }
 #else
 @:enum
@@ -82,7 +80,6 @@ abstract Action(String) to String from String
 	var BACK = "back";
 	var PAUSE = "pause";
 	var RESET = "reset";
-	var SPACE = "space";
 }
 #end
 
@@ -111,7 +108,6 @@ enum Control
 	ACCEPT;
 	BACK;
 	PAUSE;
-	SPACE;
 }
 
 enum KeyboardScheme
@@ -156,9 +152,6 @@ class Controls extends FlxActionSet
 	var _back = new FlxActionDigital(Action.BACK);
 	var _pause = new FlxActionDigital(Action.PAUSE);
 	var _reset = new FlxActionDigital(Action.RESET);
-	var _space = new FlxActionDigital(Action.SPACE);
-	var _spaceP = new FlxActionDigital(Action.SPACE);
-	var _spaceR = new FlxActionDigital(Action.SPACE);
 
 	#if (haxe >= "4.0.0")
 	var byName:Map<String, FlxActionDigital> = [];
@@ -308,20 +301,7 @@ class Controls extends FlxActionSet
 
 	inline function get_RESET()
 		return _reset.check();
-		
-	public var SPACE(get, never):Bool;
 
-	inline function get_SPACE()
-		return _space.check();	
-		
-    inline function get_SPACE_R()
-		return _spaceR.check();		
-		
-	public var SPACE_P(get, never):Bool;
-
-	inline function get_SPACE_P()
-		return _spaceP.check();
-    
 	#if (haxe >= "4.0.0")
 	public function new(name, scheme = None)
 	{
@@ -355,9 +335,6 @@ class Controls extends FlxActionSet
 		add(_back);
 		add(_pause);
 		add(_reset);
-		add(_space);
-		add(_spaceP);
-		add(_spaceR);
 
 		for (action in digitalActions)
 			byName[action.name] = action;
@@ -397,9 +374,6 @@ class Controls extends FlxActionSet
 		add(_back);
 		add(_pause);
 		add(_reset);
-		add(_space);
-		add(_spaceP);
-		add(_spaceR);
 
 		for (action in digitalActions)
 			byName[action.name] = action;
@@ -420,13 +394,6 @@ class Controls extends FlxActionSet
 		trackedInputsNOTES.push(input);
 		action.add(input);
 	}
-	
-	public function addButtonSpace(action:FlxActionDigital, button:FlxButton, state:FlxInputState)
-	{
-		var input:FlxActionInputDigitalIFlxInput = new FlxActionInputDigitalIFlxInput(button, state);
-		trackedInputsUI.push(input);
-		action.add(input);
-	}
 
 	public function addButtonUI(action:FlxActionDigital, button:FlxButton, state:FlxInputState)
 	{
@@ -441,17 +408,6 @@ class Controls extends FlxActionSet
 		inline forEachBound(Control.NOTE_DOWN, (action, state) -> addButtonNOTES(action, Hitbox.buttonDown, state));
 		inline forEachBound(Control.NOTE_LEFT, (action, state) -> addButtonNOTES(action, Hitbox.buttonLeft, state));
 		inline forEachBound(Control.NOTE_RIGHT, (action, state) -> addButtonNOTES(action, Hitbox.buttonRight, state));
-		inline forEachBound(Control.SPACE, (action, state) -> addButtonSpace(action, Hitbox.buttonSpace, state));
-	}
-	
-	public function setNewHitBox(Hitbox:FlxNewHitbox)
-	{
-		inline forEachBound(Control.NOTE_UP, (action, state) -> addButtonNOTES(action, Hitbox.buttonUp, state));
-		inline forEachBound(Control.NOTE_DOWN, (action, state) -> addButtonNOTES(action, Hitbox.buttonDown, state));
-		inline forEachBound(Control.NOTE_LEFT, (action, state) -> addButtonNOTES(action, Hitbox.buttonLeft, state));
-		inline forEachBound(Control.NOTE_RIGHT, (action, state) -> addButtonNOTES(action, Hitbox.buttonRight, state));
-		inline forEachBound(Control.NOTE_RIGHT, (action, state) -> addButtonNOTES(action, Hitbox.buttonRight, state));
-		//inline forEachBound(Control.SPACE, (action, state) -> addbuttonUI(action, Hitbox.buttonSpace, state));
 	}
 
 	public function setVirtualPadUI(VirtualPad:FlxVirtualPad, DPad:FlxDPadMode, Action:FlxActionMode)
@@ -607,7 +563,6 @@ class Controls extends FlxActionSet
 			case BACK: _back;
 			case PAUSE: _pause;
 			case RESET: _reset;
-			case SPACE: _space;
 		}
 	}
 
@@ -667,10 +622,6 @@ class Controls extends FlxActionSet
 				func(_pause, JUST_PRESSED);
 			case RESET:
 				func(_reset, JUST_PRESSED);
-			case SPACE:
-				func(_space, PRESSED);	
-				func(_spaceP, JUST_PRESSED);
-				func(_spaceR, JUST_RELEASED);
 		}
 	}
 
